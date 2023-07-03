dbe1ed7cdabe794fdf0a09f06ae947a802619000
jason <jkercher43@gmail.com>
2023-07-03 15:17:24 -0400

    some of analog...

--- analog-mcu/src/analog/analog.ino
+++ analog-mcu/src/analog/analog.ino
@@ -1,13 +1,60 @@
-
+#include <SPI.h>
+#include "pins_arduino.h"
 #include <Arduino.h>
 
+#include "common/fifo.h"
+#include "common/types.h"
+
+// looks like 14-pin SOIC??
+#define ADC1_PIN 4 // AIN6  ?
+#define ADC2_PIN 5 // AIN7  ?
+#define ADC3_PIN 9 // AIN11 ?
+#define ADC4_PIN 8 // AIN10 ?
+
+// TODO: Apparently the arduino libs do some hand
+//       holding here. These are defined for us
+//#define MOSI_PIN 11
+//#define MISO_PIN 12
+//#define SCK_PIN  13
+//#define SS_PIN   2
+
+typedef Fifo(u8, 64) Byte_Fifo;
+
+static Byte_Fifo _spi_buffer;
 
 void setup()
 {
 	Serial.begin(9600);
 	Serial.printf("Hello\r\n");
+
+	// ADC pins
+	pinMode(ADC1_PIN, INPUT);
+	pinMode(ADC2_PIN, INPUT);
+	pinMode(ADC3_PIN, INPUT);
+	pinMode(ADC4_PIN, INPUT);
+
+	// SPI pins
+	pinMode(MISO, OUTPUT);
+
+	// SPI Control Register
+	SPCR |= (1 << SPE);    // set slave mode
+	SPCR |= (1 << SPIE);   // interrupt enable
+	SPI.attachInterrupt();
+
 }
 
 void loop()
 {
+	int16_t adc_val[4] = {0,0,0,0};
+	adc_val[0] = analogRead(ADC1_PIN);
+	adc_val[1] = analogRead(ADC2_PIN);
+	adc_val[2] = analogRead(ADC3_PIN);
+	adc_val[3] = analogRead(ADC4_PIN);
+
+
+}
+
+ISR(SPI_STC_vect)
+{
+	uint8_t spi_recv = SPDR;
 }
---
+++ common/fifo.h
@@ -0,0 +1,59 @@
+#ifndef FIFO_H
+#define FIFO_H
+
+#include <stdint.h>
+
+/* NOTE: This header uses "statement expresions"
+ *       and typeof which are GNU extensions.
+ */
+
+/* Use anonymous struct or typedef */
+#define Fifo(T_, LEN_)        \
+	struct {              \
+		uint8_t head; \
+		uint8_t tail; \
+		T_ buf[LEN_]; \
+	}
+
+#define fifo_len(F_)                 (sizeof((F_).buf) / sizeof((F_).buf[0]))
+#define FIFO_IDX_ADD(F_, IDX_, OFF_) IDX_ = (IDX_ + (OFF_)) % fifo_len(F_)
+
+/* access: These "functions" operate directly on the struct
+ *         because there is no modification.
+ */
+#define fifo_available(F_)                           \
+	({                                           \
+		uint avail_ = (F_).head - (F_).tail; \
+		if (avail_ > fifo_len(F_))           \
+		{ avail_ += fifo_len(F_); }          \
+		avail_;                              \
+	})
+
+#define fifo_is_empty(F_) ((F_).head == (F_).tail)
+#define fifo_is_full(F_)  (((F_).head + 1) % fifo_len(F_) == (F_).tail)
+
+#define fifo_peek(F_)              ((F_).buf[(F_).tail])
+#define fifo_look_ahead(F_)        ((F_).buf[((F_).tail + 1) % fifo_len(F_)])
+#define fifo_idx_is_sane(F_, IDX_) ((F_).head - (F_).tail >= (IDX_) - (F_).tail)
+
+/* mutate: These "functions" take a pointer to the struct in order
+ *         to signal to the user there are mutations here.
+ */
+#define fifo_clear(F_) (F_)->tail = (F_)->head
+
+#define fifo_consume(F_, OFF_) FIFO_IDX_ADD(*(F_), (F_)->tail, OFF_)
+#define fifo_get(F_)                                               \
+	({                                                         \
+		typeof((F_)->buf[0]) res_ = (F_)->buf[(F_)->tail]; \
+		fifo_consume(F_, 1);                               \
+		res_;                                              \
+	})
+
+#define fifo_advance(F_, OFF_) FIFO_IDX_ADD(*(F_), (F_)->head, OFF_)
+#define fifo_add(F_, ITEM_)                    \
+	{                                      \
+		(F_)->buf[(F_)->head] = ITEM_; \
+		fifo_advance(F_, 1);           \
+	}
+
+#endif /* FIFO_H */
